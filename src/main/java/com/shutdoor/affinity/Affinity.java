42931685492fedea61b8a2476d613788015e9703
shutdoor <enyssa2129@gmail.com>
2023-05-12 14:37:20 -0500

    File Clean Up

--- src/main/java/com/shutdoor/affinity/Affinity.java
+++ src/main/java/com/shutdoor/affinity/Affinity.java
@@ -1,18 +1,15 @@
 package com.shutdoor.affinity;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 import static com.shutdoor.affinity.Affinity.MODID;
 
 
 @Mod(MODID)
 public class Affinity
 {
-    private static final Logger LOGGER = LogUtils.getLogger();
     public static final String MODID = "affinity";
 
     public Affinity()
